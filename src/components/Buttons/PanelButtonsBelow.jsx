accdf648b1b108b534a8eb832e4925da45958466
A-Gonzalez99 <115429034+A-Gonzalez99@users.noreply.github.com>
2024-12-05 17:05:14 +0000

    update

--- src/components/Buttons/PanelButtonsBelow.jsx
+++ src/components/Buttons/PanelButtonsBelow.jsx
@@ -5,11 +5,17 @@ import { useNavigate } from 'react-router-dom';
 function PanelButtonsBelow({icon ,text, clickCreate, clickCancel}) {
     const navigate = useNavigate();
 
+
+ 
+    if(clickCancel==null){
+        clickCancel = ()=>navigate(-1) ;
+    }
+
     return (
         <div className="panelButtonsBelow">
             <div className="panelButtonsBelowContent">
             <ButtonBelow icon={icon} text={text} click={clickCreate} clas={"buttonBelowCreate"}/>
-            <ButtonBelow icon={"close"} text={"Cancel"} click={()=>navigate(-1)} clas={"buttonBelowCancel"}/>
+            <ButtonBelow icon={"close"} text={"Cancel"} click={clickCancel} clas={"buttonBelowCancel"}/>
             </div>
       </div>
     )
